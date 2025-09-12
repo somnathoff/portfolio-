91f9449b9cda0a83d9b5bc62be597fdaf6e2498a
somnathoff <somnathpoff@gmail.com>
2025-09-12 17:06:42 +0530

    omm

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
+# build.sh
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
+
+# Start Gunicorn with correct WSGI path
+gunicorn step.wsgi:application --bind 0.0.0.0:$PORT
